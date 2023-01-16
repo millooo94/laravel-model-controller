e5491f3613e068861bbffb4077d7f388c3cf53d8
millooo94 <camillomusmeciunict@gmail.com>
2023-01-16 17:17:11 +0100

    refactor all

--- resources/views/guest/page.blade.php
+++ resources/views/guest/page.blade.php
@@ -2,24 +2,22 @@
 
 @section('content')
 <div class="container">
-    <div class="overflow-hidden">
-        <div class="row row-cols-4 g-4">
-            @foreach ($movies as $movie)
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <div class="card-header text-uppercase text-center fw-bold">
-                      {{$movie->title}}
-                    </div>
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item text-white">{{$movie->original_title}}</li>
-                      <li class="list-group-item text-white">{{$movie->nationality}}</li>
-                      <li class="list-group-item text-white">{{$movie->date}}</li>
-                      <li class="list-group-item text-white">{{$movie->vote}}</li>
-                    </ul>
-                  </div>
-            </div>
-            @endforeach
+    <div class="row row-cols-4 g-4">
+        @foreach ($movies as $movie)
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="card-header text-uppercase text-center fw-bold">
+                  {{$movie->title}}
+                </div>
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item text-white">{{$movie->original_title}}</li>
+                  <li class="list-group-item text-white">{{$movie->nationality}}</li>
+                  <li class="list-group-item text-white">{{$movie->date}}</li>
+                  <li class="list-group-item text-white">{{$movie->vote}}</li>
+                </ul>
+              </div>
         </div>
+        @endforeach
     </div>
 </div>
 @endsection
